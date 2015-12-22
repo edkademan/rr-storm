4665a4e7e66d8d3b409226edc331cf1b1ac77bcb
Ed Kademan <ed@kademan.org>
2015-12-22 05:55:37 -0500

    try some plots

--- working.r
+++ working.r
@@ -1,1 +1,16 @@
-x <- 1:10
+read_storm_data <- function() {
+  r <- readRDS("storm-data.rds")
+  r[,c("EVTYPE", "BGN_DATE", "END_DATE", "INJURIES", "FATALITIES",
+       "PROPDMG", "PROPDMGEXP", "CROPDMG", "CROPDMGEXP")]     
+}
+
+injuries_plot <- function(d = read_storm_data()) {
+  d <- filter(d, INJURIES > 0)
+  ggplot(d) +
+    geom_histogram(aes(log10(INJURIES)), bins = 30)
+}
+
+if_plot <- function(d = read_storm_data) {
+  ggplot(d) +
+    geom_point(aes(INJURIES, FATALITIES))
+}
